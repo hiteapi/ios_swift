04f4b4d4cabba212d395a418820e9a06c775f48f
Brian Mulloy <brian@landlessness.net>
2019-01-31 21:05:09 -0500

    added build status image to README page. Added workaround for XCTestCaseEntry error.

--- HelloWorld/Tests/HelloWorldTests/XCTestManifests.swift
+++ HelloWorld/Tests/HelloWorldTests/XCTestManifests.swift
@@ -1,6 +1,12 @@
 import XCTest
 
-#if !os(macOS)
+// added !os(iOS) as a workaround to this error on Travis:
+// XCTestManifests.swift:4:28: use of undeclared type 'XCTestCaseEntry'
+// fix was suggested here:
+// https://dive.github.io/swift-package-manager/ios/2019/01/20/swift_package_manager_vs_ios.html
+
+
+#if !os(macOS) && !os(iOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(HelloWorldTests.allTests),
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.com/hiteapi/ios_swift.svg?branch=master)](https://travis-ci.com/hiteapi/ios_swift)
+
 # ios_swift
 HITE API and iOS Swift
